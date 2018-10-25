0a6b9b7c42d0c97971e32a077985c0b62cad3d7d
fguerra0 <fguerra806@gmail.com>
2018-10-25 09:59:06 -0700

    First push of materials

---
+++ Assets/Scripts/Lights.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lights : MonoBehaviour
+{
+    public GameObject light;
+
+	void Start ()
+    {
+        light = GameObject.Find("Point Light");
+	}
+	
+	void Update ()
+    {
+        
+		
+	}
+
+    void OnTriggerEnter(Collider RightHandAnchor)
+    {
+        Instantiate(light);
+    }
+
+    void OnTriggerLeave(Collider RightHandAnchor)
+    {
+        Destroy(light);
+    }
+}
