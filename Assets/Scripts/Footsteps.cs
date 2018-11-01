9451a460e36608d53c76c347eaec5900e375b407
fguerra0 <fguerra806@gmail.com>
2018-10-31 20:52:22 -0700

    Attempted grabbables, colliders and footsteps

---
+++ Assets/Scripts/Footsteps.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class Footsteps : MonoBehaviour
+{
+    public float stepRate = 0.5f;
+    public float stepCooldown;
+    public AudioClip footstep;
+    public AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+    }
+    void Update()
+    {
+        stepCooldown -= Time.deltaTime;
+        if((Input.GetAxis("Horizontal") != 0f || Input.GetAxis("Vertical") != 0f) && stepCooldown < 0f)
+        {
+            audioSource.pitch = 1f + Random.Range(-0.2f, 0.2f);
+            audioSource.PlayOneShot(footstep, 0.9f);
+            stepCooldown = stepRate;
+
+        }
+    }
+}
